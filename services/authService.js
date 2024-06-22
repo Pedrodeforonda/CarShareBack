88064f8864f320d4d8d872219ed8791fd4ee6053
ManuLosta <mlostalom@gmail.com>
2024-06-22 20:35:41 -0300

    fix login

--- services/authService.js
+++ services/authService.js
@@ -1,42 +1,42 @@
-import bcrypt from 'bcrypt';
-import User from '../models/user.js';
+import User from "../models/user.js";
 
 const register = async (userData) => {
-    const { email, name, password } = userData;
+  const { email, name, password } = userData;
 
-    let user = await User.findOne({ email });
+  let user = await User.findOne({ email });
 
-    if (user) {
-        throw new Error('User already exists');
-    }
+  if (user) {
+    throw new Error("User already exists");
+  }
 
-    user = new User({
-        name,
-        email,
-        password
-    });
+  user = new User({
+    name,
+    email,
+    password,
+  });
 
-    await user.save();
+  await user.save();
 
-    return user;
+  return user;
 };
 
 const login = async (userData) => {
-    const { email, password } = userData;
+  const { email, password } = userData;
 
-    let user = await User.findOne({ email });
+  let user = await User.findOne({ email });
 
-    if (!user) {
-        throw new Error('Invalid Credentials');
-    }
+  if (!user) {
+    throw new Error("Invalid Credentials");
+  }
 
-    const isMatch = await bcrypt.compare(password, user.password);
+  const isMatch = user.password == password;
 
-    if (!isMatch) {
-        throw new Error('Invalid Credentials');
-    }
+  if (!isMatch) {
+    throw new Error("Invalid Credentials");
+  }
 
-    return user;
+  return user;
 };
 
 export default { register, login };
+
