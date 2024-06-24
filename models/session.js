fe2d64196a444968ca3a034939ed92491588269f
franciscolang <francisco.lang2209@gmail.com>
2024-06-23 22:27:26 -0300

    mqtt time

--- models/session.js
+++ models/session.js
@@ -7,6 +7,7 @@ const { Schema } = mongoose
 const sessionSchema = new Schema({
     user: { type: Schema.Types.ObjectId, ref: User },
     car: { type: Schema.Types.ObjectId, ref: Car },
+    start_time: { type: Date, default: Date.now },
     location: [Location.schema],
     distance: { type: Number, default: 0 },
     isActive: { type: Boolean, default: true },
