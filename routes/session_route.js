b3bee938f33fab4ddef928b70ac407324ceae7ad
franciscolang <francisco.lang2209@gmail.com>
2025-06-25 20:11:40 -0300

    corrected problem with sessions

--- routes/session_route.js
+++ routes/session_route.js
@@ -4,12 +4,12 @@ import Session from '../models/session.js'
 export const router = express.Router()
 
 router.get("/", async (req, res) => {
-    const sessions = await Session.find({ isActive: false })
+    const sessions = await Session.find({ isActive: false }).populate('user')
     res.json(sessions)
 })
 
 
 router.get("/:id", async (req, res) => {
-    const session = await Session.findById(req.params.id)
+    const session = await Session.findById(req.params.id).populate('user')
     res.json(session)
 })
